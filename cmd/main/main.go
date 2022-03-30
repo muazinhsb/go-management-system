44206b112388f60d623bbbed299cdcd71460adcc
muazinhsb <muazinh11@gmail.com>
2022-03-30 17:17:10 +0700

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/muazinhsb/go-management-system/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
